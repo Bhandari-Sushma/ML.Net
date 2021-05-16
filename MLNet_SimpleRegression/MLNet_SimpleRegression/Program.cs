ced7b7e59f8ed37271a28d4fbefe7a84e7f028ab
bhand <sushbhand123@gmail.com>
2021-05-16 18:26:19 +0200

    Created a new project for simple regression

---
+++ MLNet_SimpleRegression/MLNet_SimpleRegression/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML;
+using System;
+
+
+namespace MLNet_SimpleRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+        }
+    }
+}
