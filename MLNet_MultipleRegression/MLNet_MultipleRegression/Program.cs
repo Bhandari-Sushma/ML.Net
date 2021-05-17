69df5a22af8f02cc863c1c068a7ff4a2d8076a3d
bhand <sushbhand123@gmail.com>
2021-05-17 12:11:57 +0200

    Added new project - MultipleRegression

---
+++ MLNet_MultipleRegression/MLNet_MultipleRegression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLNet_MultipleRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
